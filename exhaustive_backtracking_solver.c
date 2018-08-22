4224100dd12c122b103450bd8f0c8cd5b6e43f0f
lilacnash <34063187+lilacnash@users.noreply.github.com>
2018-08-22 17:10:18 +0300

    exhaustive_backtracking should work + changed num_solutions func in game_logic.c (added current_indicators_board)

--- exhaustive_backtracking_solver.c
+++ exhaustive_backtracking_solver.c
@@ -5,6 +5,31 @@
 #include "exhaustive_backtracking_solver.h"
 #include "helper_functions.h"
 
+/*TODO: credit to https://www.geeksforgeeks.org/stack-data-structure-introduction-program/*/
+struct StackNode* newNode(int rows_index, int columns_index, int **new_solved_board, int current) {
+    struct StackNode* stackNode = (struct StackNode*) malloc(sizeof(struct StackNode));
+    (*stackNode).rows_index = rows_index;
+    (*stackNode).columns_index = columns_index;
+    (*stackNode).(**new_solved_board) = new_solved_board;
+    (*stackNode).current = current;
+    (*stackNode).next = NULL;
+    return stackNode;
+}
+int isEmpty(struct StackNode *root) {
+    return !root;
+}
+void push(struct StackNode** root, int rows_index, int columns_index, int **new_solved_board, int current) {
+    struct StackNode* stackNode = newNode(rows_index, columns_index, new_solved_board, current);
+    (*stackNode).next = *root;
+    *root = stackNode;
+}
+struct StackNode* pop(struct StackNode** root) {
+    //TODO: check if(isEmpty(*root)) before every call to pop()
+    struct StackNode* temp = *root;
+    *root = (*(*root)).next;
+    free(temp); //TODO: free only afterr we call this func
+    return temp;
+}
 int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_board, int current, int rows_columns_num,
                         int rows_per_block, int columns_per_block) {
     /*returns num of possible solutions to a given board (if the current board is solvable) using an exhaustive (deterministic) backtracking algorithm*/
@@ -14,31 +39,11 @@ int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_
     int i;
     int j;
     int p;
-
-    /*TODO: credit to https://www.geeksforgeeks.org/stack-data-structure-introduction-program/*/
-    struct StackNode {
-        int rows_index;
-        int columns_index;
-        int new_solved_board[ROWS_COLUMNS_NUM][ROWS_COLUMNS_NUM];
-        int current;
-        struct StackNode* next;
-    };
-    struct StackNode* newNode(int rows_index, int columns_index, int new_solved_board[ROWS_COLUMNS_NUM][ROWS_COLUMNS_NUM], int current) {
-        struct StackNode* stackNode = (struct StackNode*) malloc(sizeof(struct StackNode));
-        stackNode->rows_index = rows_index;
-        stackNode->columns_index = columns_index;
-        stackNode->new_solved_board[ROWS_COLUMNS_NUM][ROWS_COLUMNS_NUM] = new_solved_board[ROWS_COLUMNS_NUM][ROWS_COLUMNS_NUM];
-        stackNode->current = current;
-        stackNode->next = NULL;
-        return stackNode;
-    }
-
+    struct StackNode* root = NULL;
+    struct StackNode* tmp = NULL;
     counter = 0;
     num_of_solutions = 0;
     available_numbers = (int *)malloc(sizeof(int)*rows_columns_num);
-    for (i=0; i<rows_columns_num; i++){
-        available_numbers[i] = 0;
-    }
     while (new_solved_board[rows_index][columns_index] > 0) {
         if ((columns_index + 1) < rows_columns_num) {
             columns_index++;
@@ -48,15 +53,13 @@ int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_
         }
             /* if this is the last cell and its value is already set*/
         else if (((rows_index + 1) == rows_columns_num) && ((columns_index + 1) == rows_columns_num)) {
-            //return 0; //TODO: doesn't finish when the last cell is filled
             num_of_solutions++;
-
-
-            p = new_solved_board[rows_index][columns_index];
-            new_solved_board[rows_index][columns_index] = p + 1;
+            return num_of_solutions;
         }
     }
-
+    for (i=0; i<rows_columns_num; i++){
+        available_numbers[i] = 0;
+    }
     get_available_numbers_for_set(available_numbers, rows_index, columns_index, new_solved_board, rows_columns_num, rows_per_block, columns_per_block);
     /* creating the actual available numbers array*/
     for (i = 0; i < rows_columns_num; i++) {
@@ -64,68 +67,75 @@ int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_
             counter++;
         }
     }
-    /* backtracking from the first cell - means that the board is unsolvable, and there is no valid solution as is.*/
-    if (counter == 0 && rows_index == 0 && columns_index == 0) {
-        return -1;
-    }
+    /* backtracking from the first emptyy cell - means that the board is unsolvable, and there is no valid solution as is.*/
     if (counter == 0) {
-        return 1;
+        return num_of_solutions;
     }
     for (p = 0; p < rows_columns_num; p++) {
         if (available_numbers[p] == 0 && (p + 1) > current) {
-            new_solved_board[rows_index][columns_index] = p + 1;
+            new_solved_board[rows_index][columns_index] = p + 1; //TODO:remove? cell should still be 0 (to find another solution)
+            current = p + 1;
             break;
         }
     }
-    /* call the function with the next indexes.*/
-    if ((columns_index + 1) < rows_columns_num) {
-        if (exhaustive_backtracking(rows_index, columns_index + 1, new_solved_board, 0, rows_columns_num, rows_per_block, columns_per_block) ==
-            1) { //TODO: stackkk instead of recursion
-            for (i = rows_index; i < rows_columns_num; i++) {
-                if (i == rows_index) {
-                    for (j = columns_index; j < rows_columns_num; j++) {
-                        if (current_indicators_board[i][j] == 0) {
-                            new_solved_board[i][j] = 0;
-                        }
-                    }
-                } else {
-                    for (j = 0; j < rows_columns_num; j++) {
-                        if (current_indicators_board[i][j] == 0) {
-                            new_solved_board[i][j] = 0;
-                        }
-                    }
-                }
-            }
-            current = p + 1;
-            return exhaustive_backtracking(rows_index, columns_index, new_solved_board,
-                                           current, rows_columns_num, rows_per_block,
-                                           columns_per_block); //TODO: stackkk instead of recursion
+    push(&root, rows_index, columns_index, new_solved_board, current);
+    if ((columns_index + 1) < rows_columns_num){
+        push(&root, rows_index, columns_index + 1, new_solved_board, 0);
+    } else if ((rows_index + 1) < rows_columns_num){
+        push(&root, rows_index + 1, 0, new_solved_board, 0);
+    }
+    while(!isEmpty(root)){
+        tmp = pop(&root);
+        rows_index = (*tmp).rows_index;
+        columns_index = (*tmp).columns_index;
+        current = (*tmp).current;
+        if(current_indicators_board[rows_index][columns_index] == 0){
+            new_solved_board[rows_index][columns_index] = 0;
         }
-    } else if ((rows_index + 1) < rows_columns_num) {
-        if (exhaustive_backtracking(rows_index + 1, 0, new_solved_board, 0, rows_columns_num, rows_per_block,
-                                    columns_per_block) == 1) { //TODO: stackkk instead of recursion
-            for (i = rows_index; i < rows_columns_num; i++) {
-                if (i == rows_index) {
-                    for (j = columns_index; j < rows_columns_num; j++) {
-                        if (current_indicators_board[i][j] == 0) {
-                            new_solved_board[i][j] = 0;
-                        }
-                    }
-                } else {
-                    for (j = 0; j < rows_columns_num; j++) {
-                        if (current_indicators_board[i][j] == 0) {
-                            new_solved_board[i][j] = 0;
-                        }
-                    }
-                }
+        while (new_solved_board[rows_index][columns_index] > 0) {
+            if ((columns_index + 1) < rows_columns_num) {
+                columns_index++;
+            } else if ((rows_index + 1) < rows_columns_num) {
+                columns_index = 0;
+                rows_index++;
             }
-            current = p + 1;
-            return exhaustive_backtracking(rows_index, columns_index, new_solved_board,
-                                           current, rows_columns_num, rows_per_block,
-                                           columns_per_block); //TODO: stackkk instead of recursion
+            /*else if this is the last cell and its value is already set - increment value of last cell ||OR|| backtrack if can't increment*/
+            else if (((rows_index + 1) == rows_columns_num) && ((columns_index + 1) == rows_columns_num)) {
+                num_of_solutions++;
+                tmp = pop(&root);
+                rows_index = (*tmp).rows_index;
+                columns_index = (*tmp).columns_index;
+                current = (*tmp).current;
+                //break out of whileloop in order to increment this cell || backtrack
+            }
+        }
+        for (i=0; i<rows_columns_num; i++){
+            available_numbers[i] = 0;
+        }
+        get_available_numbers_for_set(available_numbers, rows_index, columns_index, new_solved_board, rows_columns_num, rows_per_block, columns_per_block);
+        /* creating the actual available numbers array*/
+        for (i = 0; i < rows_columns_num; i++) {
+            if (available_numbers[i] == 0 && (i + 1) > current) {
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            //new_solved_board[rows_index][columns_index] = 0; //TODO:need this?
+            continue;
+        }
+        for (p = 0; p < rows_columns_num; p++) {
+            if (available_numbers[p] == 0 && (p + 1) > current) {
+                new_solved_board[rows_index][columns_index] = p + 1;
+                current = p + 1;
+                break;
+            }
+        }
+        push(&root, rows_index, columns_index, new_solved_board, current);
+        if ((columns_index + 1) < rows_columns_num){
+            push(&root, rows_index, columns_index + 1, new_solved_board, 0);
+        } else if ((rows_index + 1) < rows_columns_num){
+            push(&root, rows_index + 1, 0, new_solved_board, 0);
         }
     }
-    /*the last cell was successfully assigned the value p+1*/
-    num_of_solutions++;
-    //return 0; //TODO: doesn't finish when the last cell is filled
+    return num_of_solutions;
 }

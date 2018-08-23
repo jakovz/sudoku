1bd520aacb9ab0ed6dcba71ea7c95ef4e56d06de
lilacnash <34063187+lilacnash@users.noreply.github.com>
2018-08-23 14:28:04 +0300

    exhaustive_backtracking updated (should work)

--- exhaustive_backtracking_solver.c
+++ exhaustive_backtracking_solver.c
@@ -38,6 +38,7 @@ int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_
     int num_of_solutions;
     int counter;
     int i;
+    int j;
     int p;
     struct StackNode* root;
     struct StackNode tmp;
@@ -50,64 +51,56 @@ int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_
         return -1;
     }
     printf("allocated available numbers");
-    while (new_solved_board[rows_index][columns_index] > 0) {
-        if ((columns_index + 1) < rows_columns_num) {
-            columns_index++;
-        } else if ((rows_index + 1) < rows_columns_num) {
-            columns_index = 0;
-            rows_index++;
-        }
-            /* if this is the last cell and its value is already set*/
-        else if (((rows_index + 1) == rows_columns_num) && ((columns_index + 1) == rows_columns_num)) {
-            num_of_solutions++;
-            return num_of_solutions;
-        }
-    }
-    for (i=0; i<rows_columns_num; i++){
-        available_numbers[i] = 0;
-    }
-    printf("after available numbers for loop\n");
-    get_available_numbers_for_set(available_numbers, rows_index, columns_index, new_solved_board, rows_columns_num, rows_per_block, columns_per_block);
-    printf("after get available numbers for set\n");
-    /* creating the actual available numbers array*/
-    for (i = 0; i < rows_columns_num; i++) {
-        if (available_numbers[i] == 0 && (i + 1) > current) {
-            counter++;
-        }
-    }
-    /* backtracking from the first emptyy cell - means that the board is unsolvable, and there is no valid solution as is.*/
-    if (counter == 0) {
-        return num_of_solutions;
-    }
-    for (p = 0; p < rows_columns_num; p++) {
-        if (available_numbers[p] == 0 && (p + 1) > current) {
-            new_solved_board[rows_index][columns_index] = p + 1;
-            current = p + 1;
-            break;
-        }
-    }
-    push(&root, rows_index, columns_index, current);
-    if ((columns_index + 1) < rows_columns_num){
-        push(&root, rows_index, columns_index + 1, 0);
-    } else if ((rows_index + 1) < rows_columns_num){
-        push(&root, rows_index + 1, 0, 0);
-    }
-    while(!isEmpty(root)){
-        tmp = pop(&root);
-        rows_index = tmp.rows_index;
-        columns_index = tmp.columns_index;
-        current = tmp.current;
-        if(current_indicators_board[rows_index][columns_index] == 0){
-            new_solved_board[rows_index][columns_index] = 0;
-        }
-        while (new_solved_board[rows_index][columns_index] > 0) {
+
+    while((!isEmpty(root)) || (rows_index==0 && columns_index==0)){
+        while((rows_index < rows_columns_num) && (columns_index < rows_columns_num)){
+            if(current_indicators_board[rows_index][columns_index] == 0){
+                if(new_solved_board[rows_index][columns_index] == 0){
+                    current = 0;
+                }
+                for (i=0; i<rows_columns_num; i++){
+                    available_numbers[i] = 0;
+                }
+                printf("after available numbers for loop\n");
+                get_available_numbers_for_set(available_numbers, rows_index, columns_index, new_solved_board, rows_columns_num, rows_per_block, columns_per_block);
+                printf("after get available numbers for set\n");
+                /* creating the actual available numbers array*/
+                for (i = 0; i < rows_columns_num; i++) {
+                    if (available_numbers[i] == 0 && (i + 1) > current) {
+                        counter++;
+                    }
+                }
+                /* backtracking from the first emptyy cell - means that the board is unsolvable, and there is no valid solution as is.*/
+                if (counter == 0) {
+                    tmp = pop(&root);
+                    rows_index = tmp.rows_index;
+                    columns_index = tmp.columns_index;
+                    current = tmp.current;
+                    /*initializing every cell after the one we just popped*/
+                    for (i=rows_index+1; i<rows_columns_num; i++){
+                        for (j=columns_index+1; j<rows_columns_num;j++){
+                            if(current_indicators_board[i][j] == 0){
+                                new_solved_board[i][j] = 0;
+                            }
+                        }
+                    }
+                    continue;
+                }
+                for (p = 0; p < rows_columns_num; p++) {
+                    if (available_numbers[p] == 0 && (p + 1) > current) {
+                        new_solved_board[rows_index][columns_index] = p + 1;
+                        current = p + 1;
+                        break;
+                    }
+                }
+                push(&root, rows_index, columns_index, current);
+            }
             if ((columns_index + 1) < rows_columns_num) {
                 columns_index++;
             } else if ((rows_index + 1) < rows_columns_num) {
                 columns_index = 0;
                 rows_index++;
-            }
-            /*else if this is the last cell and its value is already set - increment value of last cell ||OR|| backtrack if can't increment*/
+            } /*else if this is the last cell and its value is already set - increment value of last cell ||OR|| backtrack if can't increment*/
             else if (((rows_index + 1) == rows_columns_num) && ((columns_index + 1) == rows_columns_num)) {
                 num_of_solutions++;
                 tmp = pop(&root);
@@ -116,32 +109,7 @@ int exhaustive_backtracking(int rows_index, int columns_index, int **new_solved_
                 current = tmp.current;
             }
         }
-        for (i=0; i<rows_columns_num; i++){
-            available_numbers[i] = 0;
-        }
-        get_available_numbers_for_set(available_numbers, rows_index, columns_index, new_solved_board, rows_columns_num, rows_per_block, columns_per_block);
-        /* creating the actual available numbers array*/
-        for (i = 0; i < rows_columns_num; i++) {
-            if (available_numbers[i] == 0 && (i + 1) > current) {
-                counter++;
-            }
-        }
-        if (counter == 0) {
-            continue;
-        }
-        for (p = 0; p < rows_columns_num; p++) {
-            if (available_numbers[p] == 0 && (p + 1) > current) {
-                new_solved_board[rows_index][columns_index] = p + 1;
-                current = p + 1;
-                break;
-            }
-        }
-        push(&root, rows_index, columns_index, current);
-        if ((columns_index + 1) < rows_columns_num){
-            push(&root, rows_index, columns_index + 1, 0);
-        } else if ((rows_index + 1) < rows_columns_num){
-            push(&root, rows_index + 1, 0, 0);
-        }
+        //num_of_solutions++;
     }
     free(available_numbers);
     return num_of_solutions;
